647878de1465e4dd919bc61fb2bf8a1a8ca847e7
wingfeng <wingfeng@gmail.com>
2021-02-24 21:29:47 +0800

    add oauth2 file to folder

---
+++ oauth2/generates/authorize_test.go
@@ -0,0 +1,31 @@
+package generates_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/wingfeng/idx/oauth2"
+	"github.com/wingfeng/idx/oauth2/generates"
+	"github.com/wingfeng/idx/oauth2/models"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAuthorize(t *testing.T) {
+	Convey("Test Authorize Generate", t, func() {
+		data := &oauth2.GenerateBasic{
+			Client: &models.Client{
+				ID:     "123456",
+				Secret: "123456",
+			},
+			UserID:   "000000",
+			CreateAt: time.Now(),
+		}
+		gen := generates.NewAuthorizeGenerate()
+		code, err := gen.Token(context.Background(), data)
+		So(err, ShouldBeNil)
+		So(code, ShouldNotBeEmpty)
+		Println("\nAuthorize Code:" + code)
+	})
+}
